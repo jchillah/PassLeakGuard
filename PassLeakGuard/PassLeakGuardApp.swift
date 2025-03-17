7149a71170bbd075152311f39d97c581aef434ef
Jchillah <jchillah@gmail.com>
2025-03-17 18:19:58 +0100

    projekt erstellt mit Projektstruktur nach MVVM Pattern

---
+++ PassLeakGuard/PassLeakGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassLeakGuardApp.swift
+//  PassLeakGuard
+//
+//  Created by Michael Winkler on 17.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PassLeakGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
